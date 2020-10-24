a38c2e7920c8d9b5a7793637ff178b5dfc687ae0
akashgujjar <akashsgujjar@gmail.com>
2020-10-24 01:18:39 -0700

    link fix

--- bot.py
+++ bot.py
@@ -18,23 +18,52 @@ def handle_data():
     return render_template('index.html', name="hello")
 
 
-@app.route('/addRegion', methods=['POST'])
-def addRegion():
+@app.route('/login', methods=['POST', "GET"])
+def login():
+    if request.method == 'POST':
+        # gets the username from the html form
+        username = request.form['username']
+        person = "User: " + username
+        users = db.child("users").get()
+        # gets a dictionary of all the users in the database
+        temp = users.val()
+        # gets a list of the usernames from temp
+        listOfUsers = temp.keys()
+        if person in listOfUsers:
+            userData = db.child("users").child(person).get()
+            userDataValues = userData.val()
+            if username == userDataValues["name"]:
+                return render_template('profile.html', name="login success you are " + userDataValues["email"])
+        return render_template('login.html', name=None)
+    if request.method == 'GET':
+        return render_template('login.html', name=None)
+    else:
+        return render_template('index.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
     username = request.form['username']
     email = request.form['email']
     year = request.form['year']
-    addToDatabase(username, email, year)
-    return render_template('index.html', name=username)
+    person = addToDatabase(username, email, year)
+    return profile(person)
+
+
+@app.route('/profilePage')
+def profile(person):
+    users = db.child("users").child(person).get()
+    temp = users.val()
+    return render_template('login.html', name=temp)
 
 
 def addToDatabase(username, email, year):
     data = {"name": username,
             "email": email,
             "year": year}
-    db.child("users").child("User: " + username).set(data)
-
+    person = "User: " + username
+    db.child("users").child(person).set(data)
     users = db.child("users").get()
-    print(users.val())
+    return person
 
-# export FLASK_APP=bot.py
-# flask run
+# FLASK_APP=bot.py FLASK_ENV=development flask run
